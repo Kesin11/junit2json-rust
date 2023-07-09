eccd6d01966d2e879fcb34c8279b30db57d68b21
Kenta Kase <kesin1202000@gmail.com>
2023-07-09 21:50:57 +0900

    Replace skip_serializing_if to serde_with::skip_serializeing_none

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize, Serializer};
+use serde_with::skip_serializing_none;
 
 // Return true if all struct in vec are equal to Default::default()
 // fn is_all_default<T>(vec: &T) -> bool
@@ -19,117 +20,86 @@ pub enum TestSuitesOrTestSuite {
 // Reference JUnit XML Schema:
 // - https://llg.cubic.org/docs/junit/
 // - https://github.com/testmoapp/junitxml/tree/main
+#[skip_serializing_none]
 #[derive(Serialize, Deserialize, Debug, Default)]
 pub struct TestSuites {
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub testsuite: Option<Vec<TestSuite>>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub name: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub time: Option<f32>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub tests: Option<u32>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub failures: Option<u32>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub errors: Option<u32>,
 }
+
+#[skip_serializing_none]
 #[derive(Serialize, Deserialize, Debug, Default)]
 pub struct TestSuite {
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub testcase: Option<Vec<TestCase>>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub name: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub tests: Option<u32>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub failures: Option<u32>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub errors: Option<u32>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub group: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub time: Option<f32>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub disabled: Option<u32>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub skipped: Option<u32>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub timestamp: Option<String>,
 
-    #[serde(rename = "system-out", skip_serializing_if = "Option::is_none")]
+    #[serde(rename = "system-out")]
     pub system_out: Option<Vec<String>>,
-    #[serde(rename = "system-err", skip_serializing_if = "Option::is_none")]
+    #[serde(rename = "system-err")]
     pub system_err: Option<Vec<String>>,
 
-    // #[serde(skip_serializing_if = "Option::is_none")]
     // #[serde(skip_serializing_if = "is_all_default")]
     pub properties: Option<Vec<Property>>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub hostname: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub id: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub package: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub file: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub log: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub url: Option<String>,
 }
 
+#[skip_serializing_none]
 #[derive(Serialize, Deserialize, Debug, Default)]
 pub struct TestCase {
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub name: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub classname: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub assertions: Option<u32>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub time: Option<f32>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub status: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub skipped: Option<Skipped>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub error: Option<Details>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub failure: Option<Details>,
 
-    #[serde(rename = "system-out", skip_serializing_if = "Option::is_none")]
+    #[serde(rename = "system-out")]
     pub system_out: Option<Vec<String>>,
-    #[serde(rename = "system-err", skip_serializing_if = "Option::is_none")]
+    #[serde(rename = "system-err")]
     pub system_err: Option<Vec<String>>,
 
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub file: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub line: Option<u32>,
 }
 
+#[skip_serializing_none]
 #[derive(Serialize, Deserialize, Debug, Default, PartialEq)]
 pub struct Property {
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub name: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub value: Option<String>,
 }
 
+#[skip_serializing_none]
 #[derive(Serialize, Deserialize, Debug, Default)]
 pub struct Skipped {
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub message: Option<String>,
 }
 
+#[skip_serializing_none]
 #[derive(Serialize, Deserialize, Debug, Default)]
 pub struct Details {
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub message: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub r#type: Option<String>,
-    #[serde(rename(deserialize = "$value"), skip_serializing_if = "Option::is_none")]
+    #[serde(rename(deserialize = "$value"))]
     pub inner: Option<String>,
 }
 
