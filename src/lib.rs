c4dd23d87054e70635303ad55fcde65ccdaba097
Kenta Kase <kesin1202000@gmail.com>
2023-07-17 13:00:32 +0900

    Refactor: remove serde_xml_rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,24 @@
-use serde::{Deserialize, Serialize, Serializer};
+use std::default;
+use std::io;
+use std::fs;
+use quick_xml;
+use quick_xml::de;
+use serde::{Deserialize, Serialize};
 use serde_with::skip_serializing_none;
-use serde_xml_rs;
-use std::io::BufReader;
-use std::fs::File;
 
-pub mod quick;
-
-pub fn serde_xml_from_reader(reader: BufReader<File>) -> Result<TestSuitesOrTestSuite, serde_xml_rs::Error> {
-    serde_xml_rs::from_reader(reader)
+fn trim_default_items<T: default::Default + PartialEq + Clone>(vec: &mut Option<Vec<T>>) {
+    match vec {
+        Some(v) => {
+            *vec = v
+                .iter()
+                .filter(|&item| item != &Default::default())
+                .map(|item| item.clone())
+                .collect::<Vec<_>>()
+                .into();
+        },
+        None => {},
+    }
 }
-
-// Return true if all struct in vec are equal to Default::default()
-// fn is_all_default<T>(vec: &T) -> bool
-// where
-//     T: Iterator,
-//     <T as Iterator>::Item: Default + PartialEq,
-// {
-//     vec.into_iter().all(|s| s == Default::default())
-// }
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "lowercase")]
@@ -32,105 +33,151 @@ pub enum TestSuitesOrTestSuite {
 #[skip_serializing_none]
 #[derive(Serialize, Deserialize, Debug, Default)]
 pub struct TestSuites {
-    pub testsuite: Option<Vec<TestSuite>>,
+    #[serde(rename(deserialize = "@name"))]
     pub name: Option<String>,
+    #[serde(rename(deserialize = "@time"))]
     pub time: Option<f32>,
+    #[serde(rename(deserialize = "@tests"))]
     pub tests: Option<u32>,
+    #[serde(rename(deserialize = "@failures"))]
     pub failures: Option<u32>,
+    #[serde(rename(deserialize = "@errors"))]
     pub errors: Option<u32>,
+
+    pub testsuite: Option<Vec<TestSuite>>,
+}
+impl TestSuites {
+    pub fn trim_empty_items(&mut self) {
+        match &mut self.testsuite {
+            Some(testsuite) => {
+                testsuite.into_iter().for_each(|item| item.trim_empty_items())
+            }
+            None => {},
+        }
+    }
 }
 
 #[skip_serializing_none]
 #[derive(Serialize, Deserialize, Debug, Default)]
 pub struct TestSuite {
-    pub testcase: Option<Vec<TestCase>>,
+    #[serde(rename(deserialize = "@name"))]
     pub name: Option<String>,
+    #[serde(rename(deserialize = "@tests"))]
     pub tests: Option<u32>,
+    #[serde(rename(deserialize = "@failures"))]
     pub failures: Option<u32>,
+    #[serde(rename(deserialize = "@errors"))]
     pub errors: Option<u32>,
+    #[serde(rename(deserialize = "@group"))]
     pub group: Option<String>,
+    #[serde(rename(deserialize = "@time"))]
     pub time: Option<f32>,
+    #[serde(rename(deserialize = "@disabled"))]
     pub disabled: Option<u32>,
+    #[serde(rename(deserialize = "@skipped"))]
     pub skipped: Option<u32>,
+    #[serde(rename(deserialize = "@timestamp"))]
     pub timestamp: Option<String>,
+    #[serde(rename(deserialize = "@hostname"))]
+    pub hostname: Option<String>,
+    #[serde(rename(deserialize = "@id"))]
+    pub id: Option<String>,
+    #[serde(rename(deserialize = "@package"))]
+    pub package: Option<String>,
+    #[serde(rename(deserialize = "@file"))]
+    pub file: Option<String>,
+    #[serde(rename(deserialize = "@log"))]
+    pub log: Option<String>,
+    #[serde(rename(deserialize = "@url"))]
+    pub url: Option<String>,
 
     #[serde(rename = "system-out")]
     pub system_out: Option<Vec<String>>,
     #[serde(rename = "system-err")]
     pub system_err: Option<Vec<String>>,
-
-    // #[serde(skip_serializing_if = "is_all_default")]
     pub properties: Option<Vec<Property>>,
-    pub hostname: Option<String>,
-    pub id: Option<String>,
-    pub package: Option<String>,
-    pub file: Option<String>,
-    pub log: Option<String>,
-    pub url: Option<String>,
+    pub testcase: Option<Vec<TestCase>>,
+}
+impl TestSuite {
+    pub fn trim_empty_items(&mut self) {
+        trim_default_items(&mut self.system_out);
+        trim_default_items(&mut self.system_err);
+        trim_default_items(&mut self.properties);
+
+        match &mut self.testcase {
+            Some(testcase) => {
+                testcase.into_iter().for_each(|item| item.trim_empty_items())
+            }
+            None => {},
+        }
+    }
 }
 
 #[skip_serializing_none]
 #[derive(Serialize, Deserialize, Debug, Default)]
 pub struct TestCase {
+    #[serde(rename(deserialize = "@name"))]
     pub name: Option<String>,
+    #[serde(rename(deserialize = "@classname"))]
     pub classname: Option<String>,
+    #[serde(rename(deserialize = "@assertions"))]
     pub assertions: Option<u32>,
+    #[serde(rename(deserialize = "@time"))]
     pub time: Option<f32>,
+    #[serde(rename(deserialize = "@status"))]
     pub status: Option<String>,
-    pub skipped: Option<Skipped>,
-    pub error: Option<Details>,
-    pub failure: Option<Details>,
+    #[serde(rename(deserialize = "@file"))]
+    pub file: Option<String>,
+    #[serde(rename(deserialize = "@line"))]
+    pub line: Option<u32>,
 
     #[serde(rename = "system-out")]
     pub system_out: Option<Vec<String>>,
     #[serde(rename = "system-err")]
     pub system_err: Option<Vec<String>>,
-
-    pub file: Option<String>,
-    pub line: Option<u32>,
+    pub skipped: Option<Skipped>,
+    pub error: Option<Details>,
+    pub failure: Option<Details>,
 }
-
-#[skip_serializing_none]
-#[derive(Serialize, Deserialize, Debug, Default, PartialEq)]
-pub struct Property {
-    pub name: Option<String>,
-    pub value: Option<String>,
+impl TestCase {
+    pub fn trim_empty_items(&mut self) {
+        trim_default_items(&mut self.system_out);
+        trim_default_items(&mut self.system_err);
+    }
 }
 
 #[skip_serializing_none]
 #[derive(Serialize, Deserialize, Debug, Default)]
 pub struct Skipped {
+    #[serde(rename(deserialize = "@message"))]
     pub message: Option<String>,
 }
 
 #[skip_serializing_none]
 #[derive(Serialize, Deserialize, Debug, Default)]
 pub struct Details {
+    #[serde(rename(deserialize = "@message"))]
     pub message: Option<String>,
+    #[serde(rename(deserialize = "@type"))]
     pub r#type: Option<String>,
     #[serde(rename(deserialize = "$value"))]
     pub inner: Option<String>,
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use crate::{Property, is_all_default};
+#[skip_serializing_none]
+#[derive(Serialize, Deserialize, Debug, Default, PartialEq, Clone)]
+pub struct Property {
+    #[serde(rename(deserialize = "@name"))]
+    pub name: Option<String>,
+    #[serde(rename(deserialize = "@value"))]
+    pub value: Option<String>,
+}
 
-//     #[test]
-//     fn all_struct_in_vec_are_none() {
-//         let none_vec: Vec<Property> = vec![Property::default()];
-//         assert_eq!(is_all_default(&none_vec), true);
-//     }
-
-//     #[test]
-//     fn vec_is_empty() {
-//         let none_vec: Vec<Property> = vec![];
-//         assert_eq!(is_all_default(&none_vec), true);
-//     }
-
-//     #[test]
-//     fn some_struct_in_vec_are_not_none() {
-//         let none_vec: Vec<Property> = vec![Property::default(), Property { name: Some("name".to_string()), value: Some("value".to_string()) }];
-//         assert_eq!(is_all_default(&none_vec), false);
-//     }
-// }
+pub fn from_reader(reader: io::BufReader<fs::File>) -> Result<TestSuitesOrTestSuite, quick_xml::DeError> {
+    let mut root: TestSuitesOrTestSuite = de::from_reader(reader)?;
+    match root {
+        TestSuitesOrTestSuite::TestSuites(ref mut testsuites) => { testsuites.trim_empty_items() },
+        TestSuitesOrTestSuite::TestSuite(ref mut testsuite) => { testsuite.trim_empty_items() },
+    }
+    Ok(root)
+}
--- src/main.rs
+++ src/main.rs
@@ -3,32 +3,26 @@ use std::process;
 use std::fs::File;
 use std::io::BufReader;
 use junit2json;
-use junit2json::quick;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     println!("{:?}", args);
     let file = File::open(&args[1]).unwrap_or_else(|msg| {
-        eprintln!("Application error: {}", msg);
+        eprintln!("File::open error: {}", msg);
         process::exit(1);
     });
     let reader = BufReader::new(file);
-    // let testsuites: junit2json::TestSuitesOrTestSuite = junit2json::serde_xml_from_reader(reader).unwrap_or_else(|msg| {
-    //     eprintln!("serde_xml_rs error: {}", msg);
-    //     process::exit(1);
-    // });
 
-    let testsuites = quick::from_reader(reader).unwrap_or_else(|msg| {
-    // let testsuites = quick::from_str().unwrap_or_else(|msg| {
-        eprintln!("serde_xml_rs error: {}", msg);
+    let testsuites = junit2json::from_reader(reader).unwrap_or_else(|msg| {
+        eprintln!("junit2json::from_reader error: {}", msg);
         process::exit(1);
     });
 
     println!("{:#?}", testsuites);
 
     let json = serde_json::to_string_pretty(&testsuites).unwrap_or_else(|msg| {
-        eprintln!("serde_json error: {}", msg);
+        eprintln!("serde_json::to_string_pretty error: {}", msg);
         process::exit(1);
     });
     println!("{}", json);
