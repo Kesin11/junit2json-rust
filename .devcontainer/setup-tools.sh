7b51a274f7bb557f06606cb849c11c97afb1ffe4
Kenta Kase <kesin1202000@gmail.com>
2023-08-20 17:50:16 +0900

    Add wasmtime to setup-tools script

--- .devcontainer/setup-tools.sh
+++ .devcontainer/setup-tools.sh
@@ -6,6 +6,8 @@ set -x
 # nextest
 curl -LsSf https://get.nexte.st/latest/linux | tar zxf - -C ${CARGO_HOME:-~/.cargo}/bin
 
-# wasm, wasi
+# wasm, wasi build tools.
 npm install -g wasm-pack
 cargo install cargo-wasi
+# wasm runtime (wasmtime)
+curl https://wasmtime.dev/install.sh -sSf | bash
